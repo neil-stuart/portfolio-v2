70d18da6c3c1b798d22e520d556edc3c655e3821
Neil Stuart <neil.stuart11@gmail.com>
2022-12-28 23:48:58 +0000

    Attempt mobile scaling again

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,16 +8,29 @@ import About from "../components/About";
 import Projects from "../components/Projects"
 import Wordle from "../components/Wordle"
 import { useMediaQuery } from '@mui/material';
-
+import { useEffect, useState } from 'react';
 function IndexPage() {
   const isSmallScreen = useMediaQuery('(max-width:700px)');
   const isverySmallScreen = useMediaQuery('(max-width:540px)');
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const scaleAmount = (width*(47/27500)-(63/2750)).toString(3);
 
   return (
+    
     <ThemeProvider theme={theme} >
+
       <Box bg="background" sx={{ 'width': "100%", 'height': "100%" }}>
 
-        <main style={isverySmallScreen ? mobilePageStyles:(isSmallScreen ? smallPageStyles:pageStyles)} bg="background" >
+        <main style={isverySmallScreen ? {...mobilePageStyles,transform:"scale("+"0.619"/*scaleAmount*/+")"}:(isSmallScreen ? smallPageStyles:pageStyles)} bg="background" >
           <Header />
 
           <Grid gap={4} sx={{
